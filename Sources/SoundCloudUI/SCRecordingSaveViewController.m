rroy1590/CocoaSoundCloudUI

commit 25646b0de56b86c810e6b766634932e5c90f292e

remove generated sharing note

Sources/SoundCloudUI/SCRecordingSaveViewController.m
@@ -128,7 +128,6 @@
 
 #pragma mark Helpers
 - (NSString *)generatedTitle;
-- (NSString *)generatedSharingNote;
 - (NSString *)dateString;
 - (float)cellMargin;
 
@@ -1249,7 +1248,6 @@ const NSArray *allServices = nil;
     [parameters setObject:[self generatedTitle] forKey:@"track[title]"];
     [parameters setObject:(self.isPrivate) ? @"private" : @"public" forKey: @"track[sharing]"];
     [parameters setObject:(self.isDownloadable) ? @"1" : @"0" forKey: @"track[downloadable]"];
-    [parameters setObject:[self generatedSharingNote] forKey:@"track[sharing_note]"];
     [parameters setObject:@"recording" forKey:@"track[track_type]"];
 
     // sharing
@@ -1524,30 +1522,6 @@ const NSArray *allServices = nil;
             return [NSString stringWithFormat:SCLocalizedString(@"recording_from_data", @"Sounds from %@"), [self dateString]];
         }
     }
-}
-
-- (NSString *)generatedSharingNote;
-{
-    NSString *note = self.customSharingNote;
-    if (note) {
-        return note;
-    }
-    
-    if (self.title.length > 0) {
-        if (self.locationTitle.length > 0) {
-            note = [NSString stringWithFormat:SCLocalizedString(@"recording_title_at_location_name", @"%@ at %@"), self.title, self.locationTitle];
-        } else {
-            note = self.title;
-        }
-    } else {
-        if (self.locationTitle.length > 0) {
-            note = [NSString stringWithFormat:SCLocalizedString(@"recording_at_location", @"Sounds at %@"), self.locationTitle];
-        } else {
-            note = [NSString stringWithFormat:SCLocalizedString(@"recording_from_data", @"Sounds from %@"), [self dateString]];
-        }
-    }
-    
-    return note;
 }
 
 - (NSString *)dateString;

commit 97b35c0ff57809e040403fdbc2fcc253a33d60dc

Send custom sharing note to the API. Fixes bug introduced in 25646b0de56

Sources/SoundCloudUI/SCRecordingSaveViewController.m
@@ -1248,6 +1248,9 @@ const NSArray *allServices = nil;
     [parameters setObject:[self generatedTitle] forKey:@"track[title]"];
     [parameters setObject:(self.isPrivate) ? @"private" : @"public" forKey: @"track[sharing]"];
     [parameters setObject:(self.isDownloadable) ? @"1" : @"0" forKey: @"track[downloadable]"];
+    if ([self.customSharingNote length] > 0) {
+        [parameters setObject:self.customSharingNote forKey:@"track[sharing_note]"];	
+    }
     [parameters setObject:@"recording" forKey:@"track[track_type]"];
 
     // sharing