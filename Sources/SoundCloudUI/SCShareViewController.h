6dfafb9fe6b2421c8c410115d100e99b59326938
Jon Crooke <joncrooke@gmail.com>
2012-11-22 15:03:15 +0100

    Custom parameters setter

--- Sources/SoundCloudUI/SCShareViewController.h
+++ Sources/SoundCloudUI/SCShareViewController.h
@@ -40,6 +40,7 @@ typedef void(^SCSharingViewControllerComletionHandler)(NSDictionary *trackInfo, 
 - (void)setCreationDate:(NSDate *)aDate;
 - (void)setTags:(NSArray *)someTags;
 - (void)setSharingNote:(NSString *)aSharingNote;
+- (void)setCustomParameters:(NSDictionary*)dictionary;
 
 #pragma mark Foursquare
 
